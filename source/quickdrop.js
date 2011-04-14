fe36b509d85f896f2e2ca4657fed3e0fb9018393
Johan Svensson <johan@atomicplayboy.net>
2011-04-15 00:06:47 +0200

    OOPified the lot of it

--- source/quickdrop.js
+++ source/quickdrop.js
@@ -1,50 +1,55 @@
-$(document).ready(function() {
-	doBrowserWarning();
-	initHandlers();
+quickDrop = {
 
-});
+	doBrowserWarning: function() {
+		var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+		var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
-function doBrowserWarning() {
-	var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-	var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-	
-	// Show warning box if unsupported browser
-	// Only work for Chrome atm since that what I develop in
-	// Will test Firefox when the basic functionality is in.
-	if (!isChrome)
+		// Show warning box if unsupported browser
+		// Only work for Chrome atm since that what I develop in
+		// Will test Firefox when the basic functionality is in.
+		if (!isChrome)
 		$("#browserwarning").fadeIn(500);
-}
+	},
 
-function initHandlers() {
-	// The body listens for a dragged file entering
-	$("#body").bind("dragenter", onDragEnter);
+	initHandlers: function() {
+		// The body listens for a dragged file entering
+		$("#body").bind("dragenter", this.onDragEnter);
 
-	// The overlay listens for the dragleave event
-	// The ondragover event needs to be canceled in Google Chrome
-	// and Safari to allow firing the ondrop event.
-	// ref: http://help.dottoro.com/ljrkqflw.php
-	$("#overlay").bind("dragleave", onDragLeave);	
-	$("#overlay").bind("dragover", noopHandler);
+		// The overlay listens for the dragleave event
+		// The ondragover event needs to be canceled in Google Chrome
+		// and Safari to allow firing the ondrop event.
+		// ref: http://help.dottoro.com/ljrkqflw.php
+		$("#overlay").bind("dragleave", this.onDragLeave);	
+		$("#overlay").bind("dragover", this.noopHandler);
 
-	// File gets dropped
-	$("#overlay").bind("drop", onDrop);
-}
+		// File gets dropped
+		$("#overlay").bind("drop", this.onDrop);
+	},
 
-function noopHandler(evt) {
-	evt.stopPropagation();
-	evt.preventDefault();
-}
+	noopHandler: function(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+	},
 
-function onDragEnter(evt) {
-	$("#overlay").fadeIn(150);
-}
+	onDragEnter: function(evt) {
+		$("#overlay").fadeIn(150);
+	},
 
-function onDragLeave(evt) {
-	$("#overlay").fadeOut(150);
-}
+	onDragLeave: function(evt) {
+		$("#overlay").fadeOut(150);
+	},
 
-function onDrop(evt) {
-	noopHandler(evt);
-	$("#overlay").fadeOut(150);
-	$("#status").html("Uploading...")
-}
+	onDrop: function(evt) {
+		this.noopHandler(evt);
+		$("#overlay").fadeOut(150);
+	},
+	
+	setStatus: function(msg) {
+		$("#status").html(msg);		
+	}
+};
+
+$(document).ready(function() {
+	quickDrop.doBrowserWarning();
+	quickDrop.initHandlers();
+});
